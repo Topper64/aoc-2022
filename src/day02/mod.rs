d4ee997940f3f4c4a4f3c8a9f79f1297eebe9efc
Topper64 <24857330+Topper64@users.noreply.github.com>
2022-12-02 08:13:32 +0000

    Day 2 part 2

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -1,10 +1,10 @@
-use std::ops::BitOr;
+use std::ops::{BitAnd, BitOr};
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
 struct InputError;
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 enum Rps {
     Rock = 1,
     Paper = 2,
@@ -31,6 +31,19 @@ enum Outcome {
     Win = 6,
 }
 
+impl FromStr for Outcome {
+    type Err = InputError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Outcome::Loss),
+            "Y" => Ok(Outcome::Draw),
+            "Z" => Ok(Outcome::Win),
+            _ => Err(InputError),
+        }
+    }
+}
+
 impl BitOr for &Rps {
     type Output = Outcome;
 
@@ -47,10 +60,26 @@ impl BitOr for &Rps {
     }
 }
 
-fn parse(line: &str) -> (Rps, Rps) {
+impl BitAnd<&Rps> for &Outcome {
+    type Output = Rps;
+
+    fn bitand(self, other: &Rps) -> Self::Output {
+        match (self, other) {
+            (Outcome::Draw, rps) => rps.clone(),
+            (Outcome::Win, Rps::Rock) => Rps::Paper,
+            (Outcome::Win, Rps::Paper) => Rps::Scissors,
+            (Outcome::Win, Rps::Scissors) => Rps::Rock,
+            (Outcome::Loss, Rps::Rock) => Rps::Scissors,
+            (Outcome::Loss, Rps::Paper) => Rps::Rock,
+            (Outcome::Loss, Rps::Scissors) => Rps::Paper,
+        }
+    }
+}
+
+fn parse<T: FromStr>(line: &str) -> (Rps, T) {
     let (left, right) = line.split_once(" ").expect("invalid input");
     let left = left.parse().expect("invalid input");
-    let right = right.parse().expect("invalid input");
+    let right = right.parse().ok().expect("invalid input");
     (left, right)
 }
 
@@ -62,9 +91,18 @@ fn part1(input: &str) -> i32 {
         .sum()
 }
 
+fn part2(input: &str) -> i32 {
+    input
+        .lines()
+        .map(parse)
+        .map(|(them, outcome)| (&outcome & &them) as i32 + outcome as i32)
+        .sum()
+}
+
 pub fn main() {
     let input = include_str!("input.txt");
     println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
 }
 
 #[cfg(test)]
@@ -74,7 +112,22 @@ mod test {
     const INPUT: &str = include_str!("test.txt");
 
     #[test]
+    fn test_parse_moves() {
+        assert_eq!(parse("A X"), (Rps::Rock, Rps::Rock));
+    }
+
+    #[test]
+    fn test_parse_outcome() {
+        assert_eq!(parse("A X"), (Rps::Rock, Outcome::Loss));
+    }
+
+    #[test]
     fn test_part1() {
         assert_eq!(part1(INPUT), 15);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 12);
     }
 }
