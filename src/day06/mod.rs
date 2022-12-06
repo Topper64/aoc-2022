d874aacaed43d1a8bdb98952c0566f686ce83fe4
Topper64 <24857330+Topper64@users.noreply.github.com>
2022-12-06 07:45:25 +0000

    Maintain counts instead of using a set

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,14 +1,16 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 
 fn part1(input: &str) -> usize {
     let mut buffer: VecDeque<_> = VecDeque::new();
+    let mut counts: HashMap<_, _> = HashMap::new();
     for (c, i) in input.chars().zip(1..) {
         buffer.push_back(c);
+        *counts.entry(c).or_insert(0) += 1;
         if buffer.len() >= 4 {
             if buffer.len() > 4 {
-                buffer.pop_front();
+                *counts.get_mut(&buffer.pop_front().unwrap()).unwrap() -= 1;
             }
-            if buffer.iter().collect::<HashSet<_>>().len() == 4 {
+            if counts.values().filter(|c| *c == &1).count() == 4 {
                 return i;
             }
         }
