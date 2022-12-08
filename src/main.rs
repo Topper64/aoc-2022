0a057af5533cde3a270ad9d9795fac0f8e72c0fa
Topper64 <24857330+Topper64@users.noreply.github.com>
2022-12-08 08:05:02 +0000

    Prepare day 8

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day04;
 mod day05;
 mod day06;
 mod day07;
+mod day08;
 
 fn main() {
     let funcs: Vec<fn()> = vec![
@@ -15,6 +16,7 @@ fn main() {
         day05::main,
         day06::main,
         day07::main,
+        day08::main,
     ];
 
     let mut args = std::env::args();
