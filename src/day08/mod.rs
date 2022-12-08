5dd810de0bdc055c3214572ec0ebc099bf1074b6
Topper64 <24857330+Topper64@users.noreply.github.com>
2022-12-08 19:04:14 +0000

    Day 8 part 2

--- src/day08/mod.rs
+++ src/day08/mod.rs
@@ -1,4 +1,4 @@
-fn part1(input: &str) -> usize {
+fn parse_input(input: &str) -> Vec<Vec<u32>> {
     let mut forest = Vec::new();
     for line in input.lines() {
         forest.push(
@@ -8,13 +8,18 @@ fn part1(input: &str) -> usize {
         );
     }
 
+    forest
+}
+
+fn part1(input: &str) -> usize {
+    let forest = parse_input(input);
     let mut count = 0;
     for (i, row) in forest.iter().enumerate() {
         for (j, tree) in row.iter().enumerate() {
-            let left = row.iter().take(j).all(|t| t < tree);
-            let right = row.iter().skip(j + 1).all(|t| t < tree);
-            let top = forest.iter().take(i).all(|r| r[j] < *tree);
-            let bottom = forest.iter().skip(i + 1).all(|r| r[j] < *tree);
+            let left = row[..j].iter().all(|t| t < tree);
+            let right = row[j + 1..].iter().all(|t| t < tree);
+            let top = forest[..i].iter().all(|r| r[j] < *tree);
+            let bottom = forest[i + 1..].iter().all(|r| r[j] < *tree);
 
             if left || right || top || bottom {
                 count += 1;
@@ -25,9 +30,37 @@ fn part1(input: &str) -> usize {
     count
 }
 
+fn part2(input: &str) -> usize {
+    let forest = parse_input(input);
+    let h = forest.len();
+    let mut best = 0;
+    for (i, row) in forest.iter().enumerate() {
+        let w = row.len();
+        for (j, tree) in row.iter().enumerate() {
+            let left = row[..j].iter().rev().take_while(|t| t < &tree).count() + 1;
+            let right = row[j + 1..].iter().take_while(|t| t < &tree).count() + 1;
+            let top = forest[..i]
+                .iter()
+                .rev()
+                .take_while(|r| r[j] < *tree)
+                .count()
+                + 1;
+            let bottom = forest[i + 1..].iter().take_while(|r| r[j] < *tree).count() + 1;
+
+            let score = left.min(j) * right.min(w - j - 1) * top.min(i) * bottom.min(h - i - 1);
+            if score > best {
+                best = score
+            }
+        }
+    }
+
+    best
+}
+
 pub fn main() {
     let input = include_str!("input.txt");
     println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
 }
 
 #[cfg(test)]
@@ -39,5 +72,10 @@ mod test {
     #[test]
     fn test_part1() {
         assert_eq!(part1(INPUT), 21);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 8);
     }
 }
