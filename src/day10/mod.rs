4d75893091018e6debaa1cdbed1e4acfad90e008
Topper64 <24857330+Topper64@users.noreply.github.com>
2022-12-10 06:32:30 +0000

    Refactor to iterate over all xs

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,22 +1,47 @@
+use std::str::Lines;
+
+struct Program<'a> {
+    x: i32,
+    dx: Option<i32>,
+    lines: Lines<'a>,
+}
+
+impl<'a> Iterator for Program<'a> {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(x) = self.dx {
+            self.dx = None;
+            self.x += x;
+            Some(self.x)
+        } else if let Some(line) = self.lines.next() {
+            if let Some(("addx", arg)) = line.split_once(' ') {
+                self.dx = Some(arg.parse().unwrap());
+            }
+            Some(self.x)
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> From<&'a str> for Program<'a> {
+    fn from(s: &'a str) -> Self {
+        Self {
+            x: 1,
+            dx: Some(0),
+            lines: s.lines(),
+        }
+    }
+}
+
 fn part1(input: &str) -> i32 {
     let mut total = 0;
-    let mut cycle = 0;
-    let mut x = 1;
-    for line in input.lines() {
-        let (dc, dx) = match line.split_once(' ').unwrap_or_else(|| (line, "")) {
-            ("noop", "") => (1, 0),
-            ("addx", arg) => (2, arg.parse().unwrap()),
-            _ => panic!("invalid input"),
-        };
-
+    for (x, cycle) in Program::from(input).zip(1..) {
         // Add signal strength if passing the specified cycles
-        let next_cycle = cycle + dc;
-        if cycle % 40 < 20 && next_cycle % 40 >= 20 {
-            total += ((cycle / 40) * 40 + 20) * x;
+        if cycle % 40 == 20 {
+            total += ((cycle / 40) * 40 + 20) as i32 * x;
         }
-        cycle = next_cycle;
-
-        x += dx;
     }
     total
 }
